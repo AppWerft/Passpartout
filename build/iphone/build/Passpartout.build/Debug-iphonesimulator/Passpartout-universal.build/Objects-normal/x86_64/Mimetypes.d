e8b4261578454237b5f9517c9d5adc8d5e40b20c
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2015-08-11 14:03:52 +0200

        

---
+++ build/iphone/build/Passpartout.build/Debug-iphonesimulator/Passpartout-universal.build/Objects-normal/x86_64/Mimetypes.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/fuerst/Documents/Appcelerator_Studio_Workspace/Passpartout/build/iphone/Classes/Mimetypes.m \
+  /Users/fuerst/Documents/Appcelerator_Studio_Workspace/Passpartout/build/iphone/Classes/Mimetypes.h
