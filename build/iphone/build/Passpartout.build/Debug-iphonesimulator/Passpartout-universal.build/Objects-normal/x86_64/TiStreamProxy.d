8451dbead9d8f9c80fd9a0d1268f41b45ade45be
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2015-08-11 14:04:11 +0200

        

---
+++ build/iphone/build/Passpartout.build/Debug-iphonesimulator/Passpartout-universal.build/Objects-normal/x86_64/TiStreamProxy.d
@@ -0,0 +1,27 @@
+dependencies: \
+  /Users/fuerst/Documents/Appcelerator_Studio_Workspace/Passpartout/build/iphone/Classes/TiStreamProxy.m \
+  /Users/fuerst/Documents/Appcelerator_Studio_Workspace/Passpartout/build/iphone/Classes/TiStreamProxy.h \
+  /Users/fuerst/Documents/Appcelerator_Studio_Workspace/Passpartout/build/iphone/Classes/TiProxy.h \
+  /Users/fuerst/Documents/Appcelerator_Studio_Workspace/Passpartout/build/iphone/Classes/TiEvaluator.h \
+  /Users/fuerst/Documents/Appcelerator_Studio_Workspace/Passpartout/build/iphone/Classes/KrollContext.h \
+  /Users/fuerst/Documents/Appcelerator_Studio_Workspace/Passpartout/build/iphone/Classes/TiToJS.h \
+  headers/JavaScriptCore/TiCore.h headers/JavaScriptCore/Ti.h \
+  headers/JavaScriptCore/TiBase.h headers/JavaScriptCore/TiContextRef.h \
+  headers/JavaScriptCore/TiObjectRef.h \
+  headers/JavaScriptCore/TiValueRef.h \
+  headers/JavaScriptCore/WebKitAvailability.h \
+  headers/JavaScriptCore/TiStringRef.h \
+  headers/JavaScriptCore/TiStringRefCF.h \
+  /Users/fuerst/Documents/Appcelerator_Studio_Workspace/Passpartout/build/iphone/Classes/KrollCallback.h \
+  /Users/fuerst/Documents/Appcelerator_Studio_Workspace/Passpartout/build/iphone/Classes/KrollObject.h \
+  /Users/fuerst/Documents/Appcelerator_Studio_Workspace/Passpartout/build/iphone/Classes/TiBindingRunLoop.h \
+  /Users/fuerst/Documents/Appcelerator_Studio_Workspace/Passpartout/build/iphone/Classes/Tibuffer.h \
+  /Users/fuerst/Documents/Appcelerator_Studio_Workspace/Passpartout/build/iphone/Classes/TiBlob.h \
+  /Users/fuerst/Documents/Appcelerator_Studio_Workspace/Passpartout/build/iphone/Classes/TiBase.h \
+  /Users/fuerst/Documents/Appcelerator_Studio_Workspace/Passpartout/build/iphone/Classes/TiThreading.h \
+  /Users/fuerst/Documents/Appcelerator_Studio_Workspace/Passpartout/build/iphone/Classes/TiPublicAPI.h \
+  /Users/fuerst/Documents/Appcelerator_Studio_Workspace/Passpartout/build/iphone/Classes/TiUtils.h \
+  /Users/fuerst/Documents/Appcelerator_Studio_Workspace/Passpartout/build/iphone/Classes/TiDimension.h \
+  /Users/fuerst/Documents/Appcelerator_Studio_Workspace/Passpartout/build/iphone/Classes/TiColor.h \
+  /Users/fuerst/Documents/Appcelerator_Studio_Workspace/Passpartout/build/iphone/Classes/WebFont.h \
+  /Users/fuerst/Documents/Appcelerator_Studio_Workspace/Passpartout/build/iphone/Classes/TiFile.h
