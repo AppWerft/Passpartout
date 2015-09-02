dcbef166a8b1e88471aae64f0c5c53d7795bdc20
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2015-09-02 09:37:14 +0200

        

--- Resources/android/ui/bill.container.js
+++ Resources/android/ui/bill.container.js
@@ -4,17 +4,26 @@ module.exports = function() {
 	var self = require('ui/window')({
 		title : 'List of projects',
 		backgroundColor : 'white',
-		layout : 'vertical'
+		
 	});
+	var container = Ti.UI.createScrollView({layout : 'vertical',scrollType:'vertical'
+		
+	});
+	self.add(container);
 	var MaterialTextfield = require('vendor/materialtextfield');
-	var firstInput =MaterialTextfield.createView({
+	
+	var firstInput = MaterialTextfield.createView({
 		titleHint : 'TitleHint1',
 		top : 50
 	}); 
 	//firstInput.test();
-	self.add(firstInput);
-	self.add(MaterialTextfield.createView({
-		titleHint : 'TitleHint2',
+	container.add(firstInput);
+	container.add(MaterialTextfield.createView({
+		titleHint : 'Zweites Eingabefeld',
+		top : 50
+	}));
+	container.add(MaterialTextfield.createView({
+		titleHint : 'Drittes Eingabefeld',
 		top : 50
 	}));
 	return self;
