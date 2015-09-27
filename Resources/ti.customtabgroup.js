15431438b5e054a8bccaa74f2e5ad1388321269d
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2015-09-27 10:42:20 +0200

    Rolfs changes + Rainer chenges in tabgroup

--- Resources/ti.customtabgroup.js
+++ Resources/ti.customtabgroup.js
@@ -11,6 +11,7 @@ var Module = function(args) {
 	    nav = args.navigation,
 	    naviwidth = 0,
 	    screenwidth = 0;
+
 	var activeTab = nav.activeTab || 0;
 	self.add(Ti.UI.createScrollableView({
 		top : position == 'top' ? nav.height : undefined,
@@ -21,11 +22,10 @@ var Module = function(args) {
 		scrollingEnabled : false
 	}));
 	/*
-	now we go separated ways for handheld and tablet 
-	*/
+	 now we go separated ways for handheld and tablet
+	 */
 	switch (true) {
 	case args.tablet || isTablet :
-		/* tablet */	
 		self.add(Ti.UI.createScrollView({
 			top : position == 'top' ? 0 : undefined,
 			bottom : position == 'bottom' ? 0 : undefined,
@@ -43,20 +43,31 @@ var Module = function(args) {
 
 		args.tabs.forEach(function(tab, ndx) {
 			var color = ndx == nav.activeTab ? nav.activeColor : nav.color;
+			var fontFamily = ndx == nav.activeTab ? nav.fontFamilyActive : nav.fontFamily;
 			console.log(color + ' ' + ndx + '   ' + nav.activeTab);
-			self.children[0].add(Ti.UI.createLabel({
+			var navLabel = Ti.UI.createLabel({
 				width : nav.tabWidth,
+				height : Ti.UI.FILL,
 				left : nav.tabWidth * ndx,
 				textAlign : 'center',
 				itemId : ndx,
 				color : color,
 				zIndex : 99,
 				font : {
+					fontFamily : fontFamily,
 					fontSize : nav.fontSize,
-					fontWeigth : 'bold'
 				},
-				text : tab.title.toUpperCase()
-			}));
+				text : tab.title.toUpperCase(),
+			});
+			if (ndx != 0) {
+				navLabel.add(Ti.UI.createView({
+					width : '1px',
+					height : Ti.UI.FILL,
+					left : 0,
+					backgroundColor : nav.backgroundActiveColor,
+				}));
+			}
+			self.children[0].add(navLabel);
 			naviwidth += nav.tabWidth;
 		});
 		self.children[0].add(handler);
@@ -65,9 +76,16 @@ var Module = function(args) {
 				return;
 			var ndx = _e.source.itemId;
 			self.children[0].children[activeTab].color = nav.color;
+			self.children[0].children[activeTab].setFont({
+				fontFamily : nav.fontFamily
+			});
 			self.children[0].children[ndx].color = nav.activeColor;
+			self.children[0].children[ndx].setFont({
+				fontFamily : nav.fontFamilyActive
+			});
 			handler.animate({
-				left : nav.tabWidth * ndx
+				left : nav.tabWidth * ndx,
+				//duration : 500
 			}, function() {
 
 			});
@@ -88,6 +106,7 @@ var Module = function(args) {
 					break;
 				}
 			}
+			console.log(x);
 			self.children[0].setContentOffset({
 				x : x,
 				y : 0
@@ -95,12 +114,7 @@ var Module = function(args) {
 			activeTab = ndx;
 		});
 		break;
-	case  args.handheld || isHandheld:
-		/* handheld :  burgermneu */
-		/* aliase: sliding menu, burger menu, drawer men
-		/* we cannot use native Drawer or Burger menus, because dont't use native navigation like actionbar/ nanigationgroup
-		 * https://github.com/viezel/NappDrawer
-		 */
+	case args.handheld || isHandheld :
 		break;
 	}
 	return self;
