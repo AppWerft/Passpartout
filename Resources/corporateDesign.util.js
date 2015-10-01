8c145a17861e3e9048064e169c7c665856377216
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2015-10-01 13:18:56 +0200

    + ci file

---
+++ Resources/corporateDesign.util.js
@@ -0,0 +1,53 @@
+"use strict";
+/**
+ * Allgemein gültige Farbwerte und ähnliches
+ */
+module.exports = {
+	colors : {
+		yellow : '#ffcc00',
+		blue : '#0A3478',
+		red : '#ff0000',
+
+		// yellow - Varianten
+		yellowGradientStart : '#ffcd00',
+		yellowGradientEnd : '#ffda3f',
+		yellowNavActive : '#fbb602',
+
+		// red - Varianten
+		redNegative : '#ff000a', // Rot f. Kontoabgaenge
+		redNegativeLight : '#da675e',
+		redErrorBg : '#c6151c',
+
+		// blue - Varianten
+		blueHover : '#477bb5',
+		blueSwitchOn : '#8499bc',
+
+		// grey
+		textGray : '#4c4c4c',
+		textGrayLight : '#666',
+		textGrayDark : '#333',
+
+		// bgGray : '#eeeffe',
+		bgGray : '#dde4f3',
+		bgGrayLighter : '#f0f5fa',
+		bgGrayLight : '#e9eef5',
+		bgGrayDark : '#c2cee1',
+		bgGrayDarker : '#afb9c8',
+
+		// green - Varianten
+		greenPositive : '#009b0f', // Gruen f. Kontoeingaenge
+		greenPositiveLight : '#71be94',
+	},
+	font : {
+		bold : 'FrutigerLTPro-Bold',
+		italic : 'FrutigerLTPro-Italic',
+		light : 'FrutigerLTPro-Light',
+		roman : 'FrutigerLTPro-Roman',
+		icons: 'icomoon'
+		/*
+		 light : 'Roboto-Light',
+		 medium : 'Roboto-Medium',
+		 regular : 'Roboto-Regular'*/
+	},
+	borderRadius : 5,
+};
